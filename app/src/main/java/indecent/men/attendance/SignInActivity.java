d6fbdd1bc4dfe41ab3c65bafec11e9f14023e759
arvinkr19 <44765895+arvinkr19@users.noreply.github.com>
2018-11-10 13:25:30 +0530

    adapter recycleview completed

--- app/src/main/java/indecent/men/attendance/SignInActivity.java
+++ app/src/main/java/indecent/men/attendance/SignInActivity.java
@@ -1,6 +1,8 @@
 package indecent.men.attendance;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
