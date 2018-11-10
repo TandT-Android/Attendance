02d0b7f8425f0b249b469980875d99f714c025d6
arvinkr19 <44765895+arvinkr19@users.noreply.github.com>
2018-11-10 13:25:30 +0530

    adapter recycleview completed

--- app/src/main/java/indecent/men/attendance/AttendanceActivity.java
+++ app/src/main/java/indecent/men/attendance/AttendanceActivity.java
@@ -2,12 +2,23 @@ package indecent.men.attendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class AttendanceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String[] subjectData={  "Physics",
+                                "Chemistry",
+                                "Biology"};
+        String[] attendanceData={   "75%",
+                                    "80%",
+                                    "40%"};
         setContentView(R.layout.activity_attendance);
+        RecyclerView programmingList = findViewById(R.id.my_recycler_view);
+        programmingList.setLayoutManager(new LinearLayoutManager(this));
+        programmingList.setAdapter(new ProgrammingAdapter(subjectData,attendanceData));
     }
 }
