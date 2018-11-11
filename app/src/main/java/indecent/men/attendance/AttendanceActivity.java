aa171ed461aa0ca765ec84ce15043c11f42f07c4
arvinkr19 <44765895+arvinkr19@users.noreply.github.com>
2018-11-11 15:54:52 +0530

    Added Firebase database dependencies

--- app/src/main/java/indecent/men/attendance/AttendanceActivity.java
+++ app/src/main/java/indecent/men/attendance/AttendanceActivity.java
@@ -13,6 +13,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AttendanceActivity extends AppCompatActivity {
     FirebaseDatabase db = FirebaseDatabase.getInstance();
